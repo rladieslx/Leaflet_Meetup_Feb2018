e7d80f474a529998d455edb8481c8e0e3fa45a3e
isabel.lafaia <isabel.lafaia@feedzai.com>
2018-02-19 00:22:34 +0000

    Updating the R script

--- rladies_geodataWithLeaflet/RLadiesLX_Feb2018.R
+++ rladies_geodataWithLeaflet/RLadiesLX_Feb2018.R
@@ -1,11 +1,11 @@
 ## Set your working directory
+# make sure all relevant files are available there
 setwd("~/Desktop/")
-
 Sys.setenv(TZ='GMT') 
 
 ## Load all necessary packages >>> The tidyverse in packages <<<
 packages <- c('tibble', 'reshape2', 'plyr', 'dplyr', 'lubridate', 'ggplot2', 
-              'skimr', 'lubridate')
+              'skimr')
 lapply(packages, require, character.only = TRUE)
 
 # Question: what is the difference between library(package) and require(package)?
@@ -22,15 +22,26 @@ head(data, 10)
 
 str(data) # get the variables type
 
+# As you can see, not all the variables'types are properly identified.
+## Transform variables into factor variables
+
 cols = c("start.station.id", "end.station.id", "gender")
 
-data[cols] <- lapply(data[cols], factor) # transform variables into factor variables
+data[cols] <- lapply(data[cols], factor) 
 
-
+# Check the data again:
 str(data)
 
+
+
+#################################################
+######## A very quick look at TIDYVERSE: ######## 
+################################################# 
+
+
 ##1 - TIBBLE
-data_as_tibble <- as.tibble(data)  # Note: this is equivalent to as_data_frame (from dplyr)
+data_as_tibble <- as.tibble(data)  
+# Note: this is equivalent to as_data_frame (from dplyr)
 
 
 
@@ -42,9 +53,7 @@ data %>% select_if(is.numeric) %>% skimr::skim()
 
 
 
-
-
-
+## 3 - LUBRIDATE
 # lubridate is the friendly parser of datetime objects
 
 data$starttime <- ymd_hms(data$starttime)
@@ -53,14 +62,15 @@ data$stoptime <- ymd_hms(data$stoptime)
 
 
 
-## PLYR
+## 4- PLYR
 
 data_by_station <- ddply(data, .(start.station.id, usertype), summarise, 
                          latitude = mean(start.station.latitude), 
                          longitude = mean(start.station.longitude),
                          avg_trip_duration = mean(tripduration))
 
-## GGPLOT2
+
+## 5- GGPLOT2
 m <- ggplot(data=data_by_station, 
        aes(x=longitude, y=latitude)) + geom_point(aes(size=avg_trip_duration, 
                                                      color=usertype))
@@ -68,12 +78,13 @@ m <- ggplot(data=data_by_station,
 m
 
 
+# think about this map as plotting a matrix: in the x axis you have the longitude (columns), 
+# while in the y axis you have the latitude (rows).
 
 
 ## adding the Empire State building location
 
 Empire_State_building_latitude <- 40.748817
-
 Empire_State_building_longitude <- -73.985428	
 
 
@@ -95,64 +106,97 @@ data_trips <- ddply(data, .(start.station.id, end.station.id),
 
 
 
-## RESHAPE2
+## 6 - RESHAPE2
 
 ## melt allows you to frame your dataset into long format
 data2  <- melt(data_trips)
 head(data2)
 
-## popularity of starting stations
 
+
+#################################################
+################# EXERCISE:  ####################
+## map the popularity of each starting station ##
+################################################# 
+
+
+## first, define popularity
 starting_stations <- ddply(data, .(start.station.id), 
                            summarise, num_trips = length(start.station.id))
 
 
+# it's always good practice to name columns without full stops (think compatibility with python)
 colnames(starting_stations)[colnames(starting_stations) == 'start.station.id'] <- 'station_ID'
 
        
 
-
+# subset your initial dataset
 data3 <- data[ , c("start.station.id", 
                   "start.station.latitude", 
                   "start.station.longitude")]
 
+
+# merge it with latitude and longitude information
 data <- merge(x=starting_stations, y=data3, 
                            by.x = "station_ID", by.y="start.station.id", 
              all.x=TRUE)
 
-data <- unique(data)
+# remove all duplicates
+data <- unique(data) 
 
 
+# check the data
+head(data)
+
+
+
+# define where you want the "camera" to be centred on (to point at)
 lat_centre <- mean(data$start.station.latitude)
 long_centre <- mean(data$start.station.longitude)
 
 
 
 
+# load the leaflet package
 library(leaflet)
 
+
+# define a coherent normalisation for your data points
+data$size <- ((data$num_trips) - min(data$num_trips)) /(max(data$num_trips) - min(data$num_trips))*10
+# Note: this normalisation ensures that size varies between 0 and 10.
+
+
+# check the dataset
+head(data, 3)        
+
+
+# map this normalisation to a colour gradient, likeso:
 pal <- colorNumeric(
     palette = "RdYlGn",
     domain = data$size
 )
 
-data$size <- ((data$num_trips) - min(data$num_trips)) /(max(data$num_trips) - min(data$num_trips))*10
-head(data, 3)        
 
+#Credit: https://rpubs.com/kieg/toronto-volume-leaflet
+
+
+
+# always remember to think about the layering approach: 
+# think simple first, and then add more content
 m = leaflet(data) %>%   
     setView(lng = long_centre, lat = lat_centre, zoom = 10) %>%
-    addTiles() %>%  # Adds in the default OpenStreetMap map tiles - we will overwrite in this case
-    addProviderTiles("CartoDB.Positron") %>% # Overwriting with another map tile sset
-    #addProviderTiles("Stamen.Toner") %>%
+    addTiles() %>%  # default
+    #addProviderTiles("Stamen.Watercolor") %>% # another possible style
+    addProviderTiles("CartoDB.Positron")%>% 
     addCircleMarkers(lat = ~start.station.latitude, 
                      lng = ~start.station.longitude,
-                     radius = ~size,  # here we add in circles which correspond with pedestrian traffic
-                     color = ~pal(num_trips), 
+                     radius = ~size,  
+                     color = ~pal(size), 
                      fill = TRUE, 
                      opacity = .8,
                      fillOpacity = .8) %>%
-    addLegend("bottomright", pal = pal, values = ~size, # Adding a legend for colour interpretation
-              title = "Pedestrian Volume",
+    addLegend("bottomright", pal = pal, values = ~size, 
+              title = "Station popularity",
               opacity = 1
     )
 
@@ -167,4 +211,11 @@ bike <- makeIcon(
 
 
 m %>%
-    addMarkers(~NY_Stock_Exchange_longitude, ~NY_Stock_Exchange_latitude, icon = bike)
+    addMarkers(~Empire_State_building_longitude, ~Empire_State_building_latitude, 
+               icon = bike)
+
+
+
+# Now it's showtime. Pick up an interesting dataset and tell the story that data 
+# transpires in the form of a map or graph. If you want added inspiration, 
+# feel free to use the resources mentioned in the slides.
